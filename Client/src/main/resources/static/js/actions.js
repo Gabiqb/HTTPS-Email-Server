df91f02d8d15d637fe4113effd168b060ada011a
CatalinPucani <liviupucani@gmail.com>
2021-11-15 12:09:55 +0200

    added time to viewMail model

--- Client/src/main/resources/static/js/actions.js
+++ Client/src/main/resources/static/js/actions.js
@@ -21,14 +21,18 @@ $(document).ready(function() {
     $('#viewMailModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
         var sender = button.data('sender') // Extract info from data-* attributes
+        var reveiver = button.data('receiver')
         var subject = button.data('subject')
         var text = button.data('text')
+        var time = button.data('time')
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
         var modal = $(this)
-        modal.find('.modal-title').text('Mail from ' + sender)
+        modal.find('.modal-title').text('Mail')
         modal.find('#senderLabel').text('Sender: ' + sender)
+        modal.find('#receiverLabel').text('Receiver: ' + reveiver)
         modal.find('#subjectLabel').text('Subject: ' + subject)
+        modal.find('#timeLabel').text(time)
         modal.find('#message-text').text(text)
     })
 })
